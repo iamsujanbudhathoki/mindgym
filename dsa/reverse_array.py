46b3d8440fb544112f2e8e5603d76168fd5a7bfd
Sujan Budhathoki <iamsujanbudhathoki@gmail.com>
2025-09-20 10:44:30 +0545

    add: solve day1 problem

--- dsa/reverse_array.py
+++ dsa/reverse_array.py
@@ -1,8 +1,13 @@
-# Reverse an array in-place
-
 def reverse_array(array: list[int]):
-    pass
+    length = len(array)
+    for idx in range(length // 2):  # only loop until the middle
+        new_idx = length - idx - 1
 
+        # swap
+        array[idx], array[new_idx] = array[new_idx], array[idx]
+
+    print(array)
 
 
 lists = [13, -2, 37, 4, 59, 696, 9]
+reverse_array(lists)
